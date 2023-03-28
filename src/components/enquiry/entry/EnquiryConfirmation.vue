c88d5241d0e1886a708689e623b58666929c4a5a
Peter Stone <pestone@btopenworld.com>
2023-03-28 15:04:00 +0100

    Reformatting of About Me and Enquiry Confirmation components

--- src/components/enquiry/entry/EnquiryConfirmation.vue
+++ src/components/enquiry/entry/EnquiryConfirmation.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Contact form -->
-    <div class="min-h-screen bg-black py-10 sm:col-span-1 md:col-span-2 lg:col-span-3 ">
+    <div class="bg-black py-10 sm:col-span-1 md:col-span-2 lg:col-span-3 ">
         <h2 class="text-teal-600 text-3xl  tracking-tight sm:text-4xl sm:tracking-tight">
             Congratulations
         </h2>
